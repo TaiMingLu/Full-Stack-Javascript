af6b511b6abe5aac241ab4203f19e5df5edeec3e
Terry <terrytaimingl@gmail.com>
2022-09-01 12:43:22 -0400

    Add gitigonre

---
+++ BetterSleepTime/script.js
@@ -0,0 +1,24 @@
+function handleOnClick() {
+    let output = document.querySelector(".output");
+    output.style.display = "block";
+
+    let hours = document.getElementById('hours')
+    hours.innerHTML = ""
+
+
+    let now = new Date();
+
+    // 14 minutes to fall asleep
+    now.setMinutes(now.getMinutes() + 14);
+
+    for (let i = 1; i <= 6; i++) {
+        now.setMinutes(now.getMinutes() + 90);
+        hours.innerHTML += (now.toLocaleTimeString(
+            "en-us",
+            {
+                timeStyle: "short"
+            }
+        )) + "<br />";
+    }
+
+}
