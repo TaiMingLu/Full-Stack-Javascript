527d3d1c8bfa58545ea9a7d4dd59d5d93a33d0d2
TaiMingLu <terrytaimingl@gmail.com>
2022-09-06 12:33:30 -0400

    Update the script to calc sleep time based on user input

--- BetterSleepTime/script.js
+++ BetterSleepTime/script.js
@@ -14,11 +14,20 @@ function handleOnClick() {
     let hours = document.getElementById('hours')
     hours.innerHTML = ""
 
+    let hh = document.getElementById("hh").value;
+    let mm = document.getElementById("mm").value;
+    let ampm = document.getElementById("ampm").value;
+    hh = ampm === "PM" ? Number.parseInt(hh) + 12 : hh;
 
     let now = new Date();
 
     // 14 minutes to fall asleep
-    now.setMinutes(now.getMinutes() + 14);
+    // now.setMinutes(now.getMinutes() + 14);
+
+    now.setHours(hh);
+    now.setMinutes(mm);
+
+
 
     for (let i = 1; i <= 6; i++) {
         now.setMinutes(now.getMinutes() + 90);
